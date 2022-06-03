db2a3c5ece3edaaf765bd5f53be0648de2e0cfea
Scarlet Rosa <eusouscarlett@gmail.com>
2022-06-03 14:58:38 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link, Links } from 'react-router-dom';
 import ComponentTest from './Components/ComponentTest';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Router>
+      <Link to= "/">Home</Link> <br/>
+      <Link to= "/calendar">Calendar</Link> <br/>
+      <Link to= "/eventlist">Events List</Link> <br/>
 
-      <p>This is App.js</p>
-      <ComponentTest />
+        <Routes>
+          <Route path="/" element="{ <ComponentTest /> }" />
+          <Route path="/calendar" element="{ <Calendar /> }" />
+          <Route path="/eventList" element="{ <EventList /> }" />
+        </Routes>
+      </Router>
       
     </div>
   );
