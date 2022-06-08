a7320d533db70e1b18a13699836e6cdb8737b472
Scarlet Rosa <eusouscarlett@gmail.com>
2022-06-08 14:50:50 +0200

    register view

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link, Links } from 'react-router-dom';
 import ArtistList from './components/ArtistList';
 import FestivalCard from './components/FestivalCard';
-import Footer from './components/Footer.jsx'
+import Footer from './components/Footer.jsx';
 import Gigcard from './components/GigCard';
+import React, { Component } from 'react';
+import Register from './views/Register';
 
 function App() {
   return (
@@ -13,20 +15,19 @@ function App() {
         <Link to= "/">Home</Link> <br/>
         <Link to= "/calendar">Calendar</Link> <br/>
         <Link to= "/eventlist">Events List</Link> <br/>
+        <Link to= "/register">Register</Link>
 
         <Routes>
           <Route path="/" element="{ <ComponentTest /> }" />
           <Route path="/calendar" element="{ <Calendar /> }" />
           <Route path="/eventList" element="{ <EventList /> }" />
+          <Route path="/register" element="{ <Register /> }" />
         </Routes>
 
       </Router>
 
-      <Footer/>
-      
-
+    <Register/>
     </div>
   );
-}
-
+  }
 export default App;
---
+++ src/views/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/Register.css";
+
+const Register = () => {
+  return (
+    <div className="container">
+        <p>REGISTER COMPONENT</p>
+
+    </div>
+  )
+}
+
+export default Register
