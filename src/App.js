66bd9ba5b639d162f25859137ff288cae131ec78
itsberriver <jmejiaos713@gmail.com>
2022-06-08 19:11:19 +0200

    added routes to the page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link, Links } from 'react-router-dom';
-import ArtistList from './components/ArtistList';
-import FestivalCard from './components/FestivalCard';
-import Footer from './components/Footer.jsx'
-import Gigcard from './components/GigCard';
-import Button from './components/Button';
-import FormContainer from './components/FormContainer';
-import Register from './components/Register';
+import { BrowserRouter, Router,Link, Links,Routes, Route} from 'react-router-dom';
+import Login from './views/Login';
+import Home from './views/Home';
+// import FormContainer from './components/FormContainer';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
 
-        <Link to= "/">Home</Link> <br/>
-        <Link to= "/calendar">Calendar</Link> <br/>
-        <Link to= "/eventlist">Events List</Link> <br/>
-        <Link to= "/login">Login</Link> <br/>
-        <Link to= "/register">Register</Link> <br/>
+    <BrowserRouter>
+      <header > 
+        <Link to='/' >Home</Link>
+        <Link to='/login'>Login</Link>
+      </header>
 
-        <Routes>
-          <Route path="/" element="{ <ComponentTest /> }" />
-          <Route path="/calendar" element="{ <Calendar /> }" />
-          <Route path="/eventList" element="{ <EventList /> }" />
-          <Route path="/login" element="{ <Login /> }" />
-          <Route path="/register" element="{ <Register /> }" />
+      <Routes>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/login" component={ Login } />
+  
         </Routes>
 
-      </Router>
-  
-      {/* <Footer/> */}
-      <Register/>
-      
+
+      </BrowserRouter>
+
 
     </div>
+    
   );
 }
 
---
+++ src/views/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormContainer from '../components/FormContainer.jsx';
+
+function Login (){
+    return( 
+        <FormContainer/>
+    );
+}
+
+export default Login;
