f7f16e537c465f9af21da1ce75e1ca038be278a1
itsberriver <jmejiaos713@gmail.com>
2022-06-08 15:23:12 +0200

    register component created and styled

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer.jsx'
 import Gigcard from './components/GigCard';
 import Button from './components/Button';
 import FormContainer from './components/FormContainer';
+import Register from './components/Register';
 
 function App() {
   return (
@@ -15,16 +16,21 @@ function App() {
         <Link to= "/">Home</Link> <br/>
         <Link to= "/calendar">Calendar</Link> <br/>
         <Link to= "/eventlist">Events List</Link> <br/>
+        <Link to= "/login">Login</Link> <br/>
+        <Link to= "/register">Register</Link> <br/>
 
         <Routes>
           <Route path="/" element="{ <ComponentTest /> }" />
           <Route path="/calendar" element="{ <Calendar /> }" />
           <Route path="/eventList" element="{ <EventList /> }" />
+          <Route path="/login" element="{ <Login /> }" />
+          <Route path="/register" element="{ <Register /> }" />
         </Routes>
 
       </Router>
-      <FormContainer/>
+  
       {/* <Footer/> */}
+      <Register/>
       
 
     </div>
