e87b528ad9f35e1ffbbcf7d1112d41dd5b25e9fa
Scarlet Rosa <eusouscarlett@gmail.com>
2022-06-09 11:17:57 +0200

    home

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer.jsx'
 import Gigcard from './components/GigCard';
 import Button from './components/Button';
 import FormContainer from './components/FormContainer';
-import Home from './views/home';
+import Home from './views/Home';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Link to= "/eventlist">Events List</Link> <br/>
 
         <Routes>
-          <Route path="/" element="{ <ComponentTest /> }" />
+          <Route path="/" element={ <Home /> } />
           <Route path="/calendar" element="{ <Calendar /> }" />
           <Route path="/eventList" element="{ <EventList /> }" />
         </Routes>
