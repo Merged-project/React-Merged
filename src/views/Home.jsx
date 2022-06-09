760ca9fe62501ded4d09282b9b8cd22729745aa7
ginkgo <alexvid33@gmail.com>
2022-06-09 14:43:36 +0200

    add gigCard api call to Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FestivalCard from '../components/FestivalCard';
+import Gigcard from '../components/GigCard';
 
 const Home = () => {
 
     const [festivals, setFestivals] = React.useState([]);
+    const [artists, setArtists] = useState();
 
     React.useEffect(() => {
-        document.title = 'React Project';
+        document.title = 'Home';
         getData();
     }, []) // los [] impiden que se ejecute mas de una vez
 
@@ -17,14 +19,34 @@ const Home = () => {
         // console.log(festivals)
     }
 
+    const fetchArtists = async() => {
+        const artistsData = await fetch('http://127.0.0.1:8000/api/artist')
+        const artists = await artistsData.json()
+        setArtists(artists)
+    }
+
+    useEffect(() => {
+        fetchArtists();
+    },[])
+
     return (
         <div>
-            <h1>Festivals</h1>
-            {
-                festivals.map(festival =>(
-                    <FestivalCard key="festival.id" data={festival}/>
-                    ))
-            }
+            <section className='festSection'>
+                <h1>Festivals</h1>
+                {
+                    festivals.map(festival =>(
+                        <FestivalCard key="festival.id" data={festival}/>
+                        ))
+                }
+            </section>
+            <section className='artistSection'>
+                <h1>Artists</h1>
+                {
+                    artists.map(test => (
+                        <p key="test.id" artistsData={test}>{test.name}</p>
+                        ))
+                }
+            </section>
         </div>
     )
 }
