bfd7d0cdf5cbec8325a6899d61710a4fce034f01
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-10 12:34:18 +0200

    created landing screen function

--- src/App.js
+++ src/App.js
@@ -4,34 +4,43 @@ import Login from './views/Login';
 import Home from './views/Home';
 import Register from './views/RegisterView';
 import Landing from  './views/Landing';
-import './stylesheets/Login.css';
-
-// import FormContainer from './components/FormContainer';
-
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      setLoading(true)
+      setTimeout(()=> {
+        setLoading(false)
+      },3000)
+    },[])
+
   return (
     <div className="App">
+      {loading ? 
+      <Landing
+      loading = {loading}/> 
+      : <Home/>}
+      <BrowserRouter>
+        <header > 
+          <Link to='/' ></Link>
+          <Link to='/login'></Link>
+          <Link to='/register'></Link>
+          <Link to='/landing'></Link>
+        </header>
 
-    <BrowserRouter>
-      <header > 
-        <Link to='/' ></Link>
-        <Link to='/login'></Link>
-        <Link to='/register'></Link>
-        <Link to='/landing'></Link>
-      </header>
-
-      <Routes>
-          <Route exact path="/" element={ <Home/> } />
-          <Route exact path="/login" element={ <Login />} />
-          <Route exact path="/register" element={ <Register />} />
-          <Route exact path="/landing" element={ <Landing />} />
-  
-        </Routes>
+        <Routes>
+            <Route exact path="/" element={ <Home/> } />
+            <Route exact path="/login" element={ <Login />} />
+            <Route exact path="/register" element={ <Register />} />
+            <Route exact path="/landing" element={ <Landing />} />
+    
+          </Routes>
 
 
-      </BrowserRouter>
+        </BrowserRouter>
 
 
     </div>
