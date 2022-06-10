a5ca9765ec6b7a65e5fbd9ace0dedab795546555
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-10 09:38:40 +0200

    created a landing view page and styled it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Router,Link, Links,Routes, Route} from 'react-router-dom
 import Login from './views/Login';
 import Home from './views/Home';
 import Register from './views/RegisterView';
+import Landing from  './views/Landing';
 import './stylesheets/Login.css';
 
 // import FormContainer from './components/FormContainer';
@@ -18,12 +19,14 @@ function App() {
         <Link to='/' ></Link>
         <Link to='/login'></Link>
         <Link to='/register'></Link>
+        <Link to='/landing'></Link>
       </header>
 
       <Routes>
           <Route exact path="/" element={ <Home/> } />
           <Route exact path="/login" element={ <Login />} />
           <Route exact path="/register" element={ <Register />} />
+          <Route exact path="/landing" element={ <Landing />} />
   
         </Routes>
 
